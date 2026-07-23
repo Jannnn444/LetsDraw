e8eec5982f27ed703ac1828fcf564f0de6b50755
Janus <yucianhuang@gmail.com>
2026-07-23 18:15:13 +0800

    Initial Commit

---
+++ Let'sDraw/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Let'sDraw
+//
+//  Created by Janus 333 on 2026/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Let'sDraw/Let_sDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  Let_sDrawApp.swift
+//  Let'sDraw
+//
+//  Created by Janus 333 on 2026/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Let_sDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
